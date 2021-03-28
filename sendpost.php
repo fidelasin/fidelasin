58fee2777098f88c16808b77616b2a9d99daae91
fidelasin <66884583+fidelasin@users.noreply.github.com>
2021-03-28 13:53:53 +0300

    Update sendpost.php

--- sendpost.php
+++ sendpost.php
@@ -1,3 +1,4 @@
+<?php
 $useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36";
 $cookilerim=dirname(__FILE__)."/log.txt" ; 
 
